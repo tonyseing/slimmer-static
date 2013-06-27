b04ef7da5a79884527d310d0ab29c3df04748432
tony <tony@tony-VirtualBox.(none)>
2013-06-27 13:04:37 -0400

    crudely works

---
+++ slimmer.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'listen'
+require 'slim'
+
+
+# if source folder specified
+if ARGV[0]
+  Listen.to!(ARGV[0], :filter => /\.slim$/) do |modified, added| 
+    modified.each do |modified_file|
+      system("slimrb #{modified_file} > #{modified_file.gsub('.slim','')}.html")
+    end
+  end
+end
